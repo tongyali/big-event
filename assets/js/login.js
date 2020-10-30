1b0784419231c6048df6bce7ac1ceea9988cf0d9
童雅莉 <1046840300@qq.com>
2020-10-30 14:33:43 +0800

    密码校正

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,10 @@ $(function () {
         $(".login-box").show()
         $(".reg-box").hide()
     })
+    let form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ]
+    })
 })
