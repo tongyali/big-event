a89dc1826822f8d0d0d9f287536f1fc5aa492a62
童雅莉 <1046840300@qq.com>
2020-11-01 17:25:05 +0800

    修改头像

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,18 +1,35 @@
 $(function () {
     let form = layui.form
+    let layer = layui.layer
     form.verify({
         pwd: [
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
         ],
         newPwd: function (value) {
-            if (value == $("[name=oldpassword]").val()) {
+            if (value == $("[name=oldPwd]").val()) {
                 return '原密码与新密码相同'
             }
         },
         samePwd: function (value) {
-            if (value !== $("[name=newpassword]").val()) {
+            if (value !== $("[name=newPwd]").val()) {
                 return '两次输入密码不相同'
             }
         }
+    })
+    // 重置密码
+    $(".layui-form").on("submit", function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('重置密码失败')
+                }
+                layer.msg('重置密码成功')
+                $('.layui-form')[0].reset()
+            }
+        })
     })
 })
