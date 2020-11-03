12af1889bd3ecd46684cb54e80319e3fffc13005
童雅莉 <1046840300@qq.com>
2020-11-03 15:13:04 +0800

    发表文章

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -11,7 +11,7 @@ $(function () {
             method: 'GET',
             url: '/my/article/cates',
             success: function (res) {
-                console.log(res);
+                // console.log(res);
                 if (res.status !== 0) {
                     return layer.msg('获取文章分类失败')
                 }
@@ -39,7 +39,7 @@ $(function () {
     })
     // 上传图片 监听file的change事件
     $("#file-btn").on("change", function (e) {
-        console.log(e);
+        // console.log(e);
         // 拿到用户选择的文件
         let files = e.target.files
         // 创建对应的url地址
@@ -52,5 +52,50 @@ $(function () {
             .attr('src', newImgURL) // 重新设置图片路径
             .cropper(options) // 重新初始化裁剪区域
     })
+    // 定义状态变量 默认已发布
+    let art_state = '已发布'
+    // 点击存为草稿状态变为草稿
+    $("#draft").click(function () {
+        art_state = '草稿'
+    })
+    // 监听表单的提交事件
+    $("#form-pub").on("submit", function (e) {
+        // console.log(1111);
+        e.preventDefault()
 
+        //    创建FormData对象 获取表单内容  
+        let fd = new FormData($(this)[0]) //将jquery对象转为dom对象
+        // 将文件状态添加到FormData对象中
+        fd.append('state', art_state)
+        // 将裁剪后的图片输出为文件
+        $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) { // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 将裁剪的图片文件添加到FormData中
+                fd.append("cover_img", blob)
+                publishArticle(fd)
+            })
+    })
+    // 发表文章
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg("发表文章失败")
+                }
+                layer.msg("发表文章成功!")
+                location.href = "/article/art_list.html"
+            }
+        })
+    }
 })
