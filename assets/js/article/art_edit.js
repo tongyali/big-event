4ccd49257eff0e7ae5cdf95ce3ccc59a3264d382
童雅莉 <1046840300@qq.com>
2020-11-04 11:25:07 +0800

    分类渲染

--- assets/js/article/art_edit.js
+++ assets/js/article/art_edit.js
@@ -10,16 +10,13 @@ $(function () {
             console.log(res);
             // 渲染页面
             form.val('form-pub', res.data)
+            setTimeout(function () {
+                $("[name=cate_id]").find(`option[value=${res.data.cate_id}]`).prop("selected", true)
+                form.render()
+            }, 1000)
+
         }
     })
     // 获取文章类别
-    $.ajax({
-        method: 'GET',
-        url: '/my/article/cates' + Id,
-        success: function (res) {
-            console.log(res);
-            // 渲染页面
-            // form.val('form-pub', res.data)
-        }
-    })
+
 })
